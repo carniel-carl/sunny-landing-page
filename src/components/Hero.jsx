ebbf8a90ad3f97144e4e9e95b965b6c14c9ce4c1
Carniel <carlnmugha1@gmail.com>
2023-07-03 19:03:01 +0100

    adjust sizes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import NavBar from "./NavBar";
 
-import mobileBg from "./../assets/images/mobile/image-header.jpg";
-import Bg from "./../assets/images/desktop/image-header.jpg";
 import arrow from "./../assets/images/icon-arrow-down.svg";
 
 import "./../assets/styles/Hero.scss";
@@ -17,11 +15,6 @@ const Hero = () => {
       <div className="hero__arrow">
         <img src={arrow} alt="arrow icon" />
       </div>
-
-      <picture className="bg">
-        <source media="(max-width: 500px)" srcSet={mobileBg} />
-        <img src={Bg} alt="Orange background" />
-      </picture>
     </header>
   );
 };
